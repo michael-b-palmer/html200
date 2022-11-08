34cd98ad0556cdd8e5c2830198ee0747fa6e5268
Michael Palmer <michael.palmer@docusign.com>
2022-11-08 00:21:25 -0800

    Revert "Create countdown.js"
    This reverts commit d0f088a51470c08d33aa8d83666a18c734f500ca.

--- lesson02/countdown.js
+++
@@ -1,8 +0,0 @@
-console.log("Gonna eat some tacos!");
-for (let remainingtacos = 6; remainingtacos > -1; remainingtacos--) {
-  if (remainingtacos > 0) {
-    console.log(remainingtacos + " Crunch!");
-  } else {
-    console.log(remainingtacos + " BURP!");
-  }
-}
